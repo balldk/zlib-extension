687b567d80e6eaf031ee699c338e2cd04c958a01
balldk <pr0f3ss0r.b@gmail.com>
2026-03-09 19:19:42 -0400

    feat: update urls

--- src/lib/book/consts.ts
+++ src/lib/book/consts.ts
@@ -1,3 +1,3 @@
-export const ZLIB_LOGIN_URL = 'https://singlelogin.re/login.php'
-export const ZLIB_LOGOUT_URL = 'https://singlelogin.re/logout.php'
-export const ZLIB_URL = 'https://singlelogin.re'
+export const ZLIB_LOGIN_URL = 'https://z-library.sk/login.php'
+export const ZLIB_LOGOUT_URL = 'https://z-library.sk/logout.php'
+export const ZLIB_URL = 'https://z-library.sk'
