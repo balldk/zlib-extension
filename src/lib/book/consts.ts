940ae845ceaa57664b5ad4b6176e93fe6deda905
ductrl <lytriducgp@gmail.com>
2026-03-09 20:14:56 -0400

    Fix broken login and logout URLs

--- src/lib/book/consts.ts
+++ src/lib/book/consts.ts
@@ -1,3 +1,3 @@
-export const ZLIB_LOGIN_URL = 'https://z-library.sk/login.php'
-export const ZLIB_LOGOUT_URL = 'https://z-library.sk/logout.php'
+export const ZLIB_LOGIN_URL = 'https://z-library.sk/login'
+export const ZLIB_LOGOUT_URL = 'https://z-library.sk/logout'
 export const ZLIB_URL = 'https://z-library.sk'
